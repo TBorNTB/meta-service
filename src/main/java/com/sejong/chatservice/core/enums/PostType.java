5473d538e76caecf3af607c01be3ab4fd477ba94
PlusUltraCode <kkd06144@naver.com>
2025-07-23 13:16:13 +0900

    refactor: Update PostType enum for extensibility or value change

--- src/main/java/com/sejong/chatservice/core/enums/PostType.java
+++ src/main/java/com/sejong/chatservice/core/enums/PostType.java
@@ -1,5 +1,8 @@
 package com.sejong.chatservice.core.enums;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "게시글 타입 (PROJECT, ARCHIVE 등)")
 public enum PostType {
     PROJECT,
     ARCHIVE,
