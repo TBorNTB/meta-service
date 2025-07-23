bceccfacc8edc728cb160a223d7725719c4cddcc
PlusUltraCode <kkd06144@naver.com>
2025-07-23 13:16:21 +0900

    test: Fix or enhance LikeControllerTest logic and assertions

--- src/test/java/com/sejong/chatservice/application/postlike/controller/LikeControllerTest.java
+++ src/test/java/com/sejong/chatservice/application/postlike/controller/LikeControllerTest.java
@@ -1,4 +1,12 @@
+package com.sejong.chatservice.application.postlike.controller;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(LikeController.class)
+@AutoConfigureMockMvc
 class LikeControllerTest {
-  
+
 }
