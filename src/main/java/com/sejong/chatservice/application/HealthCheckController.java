3cae05957901772b65260e7d6a37c6173f92c217
PlusUltraCode <kkd06144@naver.com>
2025-07-23 13:15:44 +0900

    chore: Add HealthCheckController and Swagger configuration

--- src/main/java/com/sejong/chatservice/application/HealthCheckController.java
+++ src/main/java/com/sejong/chatservice/application/HealthCheckController.java
@@ -1,4 +1,16 @@
 package com.sejong.chatservice.application;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/health")
 public class HealthCheckController {
+
+    @GetMapping
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("ok");
+    }
 }
--- src/main/java/com/sejong/chatservice/application/config/SwaggerConfig.java
+++ src/main/java/com/sejong/chatservice/application/config/SwaggerConfig.java
@@ -1,4 +1,23 @@
 package com.sejong.chatservice.application.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        servers = {
+                @Server(url = "/chat-service"),
+                @Server(url = "/")
+        },
+        info = @Info(
+                title = "Chatting API",
+                version = "v1",
+                description = "Chatting API 문서입니다."
+        )
+)
+@Configuration
 public class SwaggerConfig {
+
+
 }
