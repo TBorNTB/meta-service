a47ceb4b01ae30dfd436072e0512ddfd838119a4
PlusUltraCode <kkd06144@naver.com>
2025-07-27 20:00:12 +0900

    #3 chore: build 설정 및 테스트 환경 구성 변경

--- src/test/java/com/sejong/chatservice/application/config/MockBeansConfig.java
+++ src/test/java/com/sejong/chatservice/application/config/MockBeansConfig.java
@@ -1,6 +1,7 @@
 package com.sejong.chatservice.application.config;
 
 import com.sejong.chatservice.application.comment.service.CommentService;
+import com.sejong.chatservice.application.postlike.service.LikeService;
 import org.springframework.context.annotation.Bean;
 
 import static org.mockito.Mockito.mock;
@@ -10,6 +11,11 @@ public class MockBeansConfig {
     @Bean
     public CommentService commentService() {
         return mock(CommentService.class);
+    }
+
+    @Bean
+    public LikeService likeService() {
+        return mock(LikeService.class);
     }
 }
 
