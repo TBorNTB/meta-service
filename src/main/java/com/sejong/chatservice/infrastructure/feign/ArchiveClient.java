f2fd0b071bbaef29551b699fbf4ff6f3441aeaa4
PlusUltraCode <kkd06144@naver.com>
2025-07-29 17:26:08 +0900

    #5 feat: 외부 서비스 연동을 위한 FeignClient(Archive, Project, User) 추가

--- src/main/java/com/sejong/chatservice/infrastructure/feign/ArchiveClient.java
+++ src/main/java/com/sejong/chatservice/infrastructure/feign/ArchiveClient.java
@@ -1,4 +1,13 @@
 package com.sejong.chatservice.infrastructure.feign;
 
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name="archive-service", path="/internal/archive")
 public interface ArchiveClient {
+
+    @GetMapping("/check/{archiveId}")
+    ResponseEntity<Boolean> checkArchive(@PathVariable("archiveId") Long archiveId);
 }
