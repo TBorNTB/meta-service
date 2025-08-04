2581731cba0036f9b8602797e9a879881e530983
PlusUltraCode <kkd06144@naver.com>
2025-08-04 15:49:56 +0900

    #5 chore: Feign Client 디렉토리 client로 변경

--- src/main/java/com/sejong/chatservice/infrastructure/client/ArchiveClient.java
+++ src/main/java/com/sejong/chatservice/infrastructure/client/ArchiveClient.java
@@ -1,4 +1,4 @@
-package com.sejong.chatservice.infrastructure.feign;
+package com.sejong.chatservice.infrastructure.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
