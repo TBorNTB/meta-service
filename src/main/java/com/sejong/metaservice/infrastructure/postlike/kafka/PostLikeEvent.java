d5887e49ed0cc8331e3e0171ee70eee24ab2e0c9
PlusUltraCode <kkd06144@naver.com>
2025-08-27 16:29:19 +0900

    #12 feat: PostLikeEvent DTO 및 이벤트 퍼블리셔 구현

--- src/main/java/com/sejong/metaservice/infrastructure/postlike/kafka/PostLikeEvent.java
+++ src/main/java/com/sejong/metaservice/infrastructure/postlike/kafka/PostLikeEvent.java
@@ -1,4 +1,26 @@
 package com.sejong.metaservice.infrastructure.postlike.kafka;
 
+import com.sejong.metaservice.core.common.enums.PostType;
+import com.sejong.metaservice.core.postlike.domain.PostLike;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class PostLikeEvent {
+    private Long postId;
+    private PostType postType;
+    private Long likeCount;
+
+    public static PostLikeEvent of(PostType postType, Long postId, Long likeCount) {
+        return PostLikeEvent.builder()
+                .postType(postType)
+                .postId(postId)
+                .likeCount(likeCount)
+                .build();
+    }
 }
