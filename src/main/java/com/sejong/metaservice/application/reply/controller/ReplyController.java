d035b1ddcf455f1db3f9073293e55ea658d19b3c
Sumi Jeong <125195487+sigmaith@users.noreply.github.com>
2025-10-12 11:39:56 +0900

    chore: 답글 api 설명 추가

--- src/main/java/com/sejong/metaservice/application/reply/controller/ReplyController.java
+++ src/main/java/com/sejong/metaservice/application/reply/controller/ReplyController.java
@@ -8,6 +8,8 @@ import com.sejong.metaservice.core.common.pagination.CursorPageRequest;
 import com.sejong.metaservice.core.common.pagination.CursorPageResponse;
 import com.sejong.metaservice.core.reply.command.ReplyCreateCommand;
 import com.sejong.metaservice.core.reply.domain.Reply;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -24,12 +26,14 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "대댓글 API", description = "댓글에 대한 대댓글 관련 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/reply")
 public class ReplyController {
     private final ReplyService replyService;
 
+    @Operation(summary = "대댓글 작성", description = "특정 댓글에 대댓글을 작성합니다")
     @PostMapping("/{commentParentId}")
     public ResponseEntity<ReplyCommentResponse> createReplyComment(
             @RequestHeader("X-User-Id") String username,
@@ -42,6 +46,7 @@ public class ReplyController {
                 .body(response);
     }
 
+    @Operation(summary = "대댓글 목록 조회", description = "특정 댓글의 대댓글 목록을 커서 기반 페이징으로 조회합니다")
     @GetMapping("/{commentParentId}")
     public ResponseEntity<CursorPageResponse<List<Reply>>> showReplyComments(
             @PathVariable(name="commentParentId") Long commentParentId,
@@ -53,6 +58,7 @@ public class ReplyController {
                 .body(response);
     }
 
+    @Operation(summary = "대댓글 수정", description = "작성한 대댓글의 내용을 수정합니다")
     @PatchMapping("/{replyId}")
     public ResponseEntity<ReplyCommentResponse> updateReplyComment(
             @RequestHeader("X-User-Id") String username,
@@ -64,6 +70,7 @@ public class ReplyController {
                 .body(response);
     }
 
+    @Operation(summary = "대댓글 삭제", description = "작성한 대댓글을 삭제합니다")
     @DeleteMapping("/{replyId}")
     public ResponseEntity<ReplyCommentResponse> deleteReplyComment(
             @RequestHeader("X-User-Id") String username,
