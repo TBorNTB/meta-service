c3220a4f8eb835caf63e3d48f2c415cb1eb16939
Sumi Jeong <125195487+sigmaith@users.noreply.github.com>
2025-12-13 11:41:51 +0900

    refactor(like): 동기화 스케줄러 버그 가시화

--- src/main/java/com/sejong/metaservice/domain/like/service/LikeScheduler.java
+++ src/main/java/com/sejong/metaservice/domain/like/service/LikeScheduler.java
@@ -18,30 +18,24 @@ public class LikeScheduler {
 
     @Scheduled(cron = "0 0 3 * * *", zone = "Asia/Seoul") // 매일 오전 3시
     public void runRedisLikeSyncJob() {
-        try {
-            log.info("Redis 좋아요 동기화 작업 시작 - 통계 쿼리 방식");
-            
-            // 통계 쿼리로 좋아요 수 조회
-            Map<String, Long> likeCountStatistics = likeRepository.getLikeCountStatistics();
-            
-            // Redis에 일괄 설정
-            likeCountStatistics.forEach(redisService::setCount);
-            
-            log.info("Redis 좋아요 동기화 작업 완료: {} 건 처리", likeCountStatistics.size());
+        log.info("Redis 좋아요 동기화 작업 시작 - 통계 쿼리 방식");
 
-            /**
-             * TODO: 접근이 잦은 게시물에 대한 전략적 캐싱을 고려해야합니다.(postType, postId, userId)
-             *
-             * JobParameters jobParameters = new JobParametersBuilder()
-             *                     .addLong("timestamp", System.currentTimeMillis()) // 매 실행 시마다 다른 값으로 중복 방지
-             *                     .toJobParameters();
-             *
-             *             jobLauncher.run(postLikeRedisSyncJob, jobParameters);
-             */
+        // 통계 쿼리로 좋아요 수 조회
+        Map<String, Long> likeCountStatistics = likeRepository.getLikeCountStatistics();
 
-        } catch (Exception e) {
-            log.error("Redis 좋아요 통계 캐싱 작업 실패", e);
-            throw new RuntimeException("Redis 좋아요 배치 작업 실패", e);
-        }
+        // Redis에 일괄 설정
+        likeCountStatistics.forEach(redisService::setCount);
+
+        log.info("Redis 좋아요 동기화 작업 완료: {} 건 처리", likeCountStatistics.size());
+
+        /**
+         * TODO: 접근이 잦은 게시물에 대한 전략적 캐싱을 고려해야합니다.(postType, postId, userId)
+         *
+         * JobParameters jobParameters = new JobParametersBuilder()
+         *                     .addLong("timestamp", System.currentTimeMillis()) // 매 실행 시마다 다른 값으로 중복 방지
+         *                     .toJobParameters();
+         *
+         *             jobLauncher.run(postLikeRedisSyncJob, jobParameters);
+         */
     }
 }
