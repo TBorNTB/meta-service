8266c7f178e35f2c90e1c89c91494d54d0b4024b
Sumi Jeong <125195487+sigmaith@users.noreply.github.com>
2025-12-16 16:43:51 +0900

    chore(like): swagger 주석 추가

--- src/main/java/com/sejong/metaservice/domain/like/controller/LikeController.java
+++ src/main/java/com/sejong/metaservice/domain/like/controller/LikeController.java
@@ -5,6 +5,7 @@ import com.sejong.metaservice.domain.like.dto.response.LikeRes;
 import com.sejong.metaservice.domain.like.service.LikeService;
 import com.sejong.metaservice.support.common.enums.PostType;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "좋아요 API", description = "게시물 좋아요 관련 API")
 @RestController
 @RequestMapping("/api/like")
 @RequiredArgsConstructor
